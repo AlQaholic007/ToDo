45b68700682b96ff74ce4793759e1be79ead5943
Soham Parekh <soham@sohams-mbp.lan>
2019-10-06 07:55:13 +0530

    final

--- todo/settings.py
+++ todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-//import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,4 +138,4 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
-//django_heroku.settings(locals())
+django_heroku.settings(locals())
